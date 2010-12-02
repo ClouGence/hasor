37f58921d3da307906cc045eb6d7751a3b5864b4
zyc <zyc@hasor.net>
2010-12-02 09:32:51 +0000

    1.递交hypha组建的测试代码和测试配置文件。
    git-svn-id: http://127.0.0.1:9090/svn/more_svn@160 f2e7b36f-b0e8-e445-a25c-5efde7ec30fc

--- Release branches/more_1.x/src/test_src/org/test/more/beans/define/XmlTest.java
+++ Release branches/more_1.x/src/test_src/org/test/more/beans/define/XmlTest.java
@@ -14,12 +14,20 @@
  * limitations under the License.
  */
 package org.test.more.beans.define;
-import java.io.IOException;
-import java.io.InputStream;
-import javax.xml.stream.XMLStreamException;
 import org.junit.Test;
+import org.more.hypha.ApplicationContext;
+import org.more.hypha.Event;
+import org.more.hypha.EventListener;
 import org.more.hypha.beans.AbstractBeanDefine;
-import org.more.hypha.configuration.XmlConfiguration;
+import org.more.hypha.beans.AbstractPropertyDefine;
+import org.more.hypha.context.XmlDefineResource;
+import org.more.hypha.event.AddBeanDefineEvent;
+import org.more.hypha.event.AddPluginEvent;
+import org.more.hypha.event.ClearDefineEvent;
+import org.more.hypha.event.Config_LoadResourceEvent;
+import org.more.hypha.event.Config_LoadedXmlEvent;
+import org.more.hypha.event.Config_LoadingXmlEvent;
+import org.more.hypha.event.ReloadDefineEvent;
 /**
  * ²âÊÔÁËnamespace°ü,define°ü
  * @version 2010-9-21
@@ -27,13 +35,49 @@ import org.more.hypha.configuration.XmlConfiguration;
  */
 public class XmlTest {
     @Test
-    public void test() throws XMLStreamException, IOException {
-        InputStream in = XmlConfiguration.class.getResourceAsStream("/org/test/more/beans/define/beans-test-config.xml");
-        XmlConfiguration con = new XmlConfiguration(in);
-        AbstractBeanDefine define = con.getBeanDefine("org.more.testTemplateBean");
+    public void test() throws Throwable {
+        EventListener listener = new EventListener() {
+            public void onEvent(Event event) {
+                System.out.println("Event:" + event.getClass());
+                if (event instanceof AddBeanDefineEvent) {
+                    AddBeanDefineEvent ade = (AddBeanDefineEvent) event;
+                    AbstractBeanDefine define = ade.getDefine();
+                    System.out.println(define.getID());
+                    //
+                } else if (event instanceof Config_LoadResourceEvent) {
+                    Config_LoadResourceEvent lre = (Config_LoadResourceEvent) event;
+                    System.out.println(lre.getResource());
+                }
+            }
+        };
+        System.out.println("start...");
+        XmlDefineResource config = XmlDefineResource.newInstanceByNew();
+        config.getEventManager().addEventListener(AddBeanDefineEvent.class, listener);
+        config.getEventManager().addEventListener(AddPluginEvent.class, listener);
+        config.getEventManager().addEventListener(ClearDefineEvent.class, listener);
+        config.getEventManager().addEventListener(Config_LoadedXmlEvent.class, listener);
+        config.getEventManager().addEventListener(Config_LoadingXmlEvent.class, listener);
+        config.getEventManager().addEventListener(ReloadDefineEvent.class, listener);
+        config.getEventManager().addEventListener(Config_LoadResourceEvent.class, listener);
         //
-        //        AopBeanDefinePlugin aopDefine = (AopBeanDefinePlugin) define.getPlugin(AopBeanDefinePlugin.AopPluginName);
-        //        System.out.println(aopDefine.getAopConfig().getName());
-        System.out.println(define.getMethods());
+        config.addSource("/org/test/more/beans/define/property-test-config.xml");
+        config.addSource("/org/test/more/beans/define/collection-test-config.xml");
+        config.addSource("/org/test/more/beans/define/beans-test-config.xml");
+        config.addSource("/org/test/more/beans/define/aop-test-config.xml");
+        config.loadDefine();
+        //
+        ApplicationContext app = config.buildApp(null);
+        //DefineResource con = config.build(null, null);
+        //
+        for (String bean : config.getBeanDefineNames()) {
+            if (bean.equals("org.more.rb_1") == true)
+                System.out.println();
+            System.out.println("--getBean:" + bean);
+            AbstractBeanDefine define = config.getBeanDefine(bean);
+            System.out.println(define);
+            for (AbstractPropertyDefine pd : define.getPropertys())
+                System.out.println(pd.getMetaData());
+        }
+        System.out.println("end!");
     }
 }
