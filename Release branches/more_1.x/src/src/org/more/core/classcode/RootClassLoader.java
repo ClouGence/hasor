6948be2afe4c98e556f497be2cbd53328c0d5bdc
zyc <zyc@hasor.net>
2011-05-12 10:38:36 +0000

    1.新增功能，使通过classcode组建生成的字节码可以通过其classLoader的getResourceAsStream获取到。
    git-svn-id: http://127.0.0.1:9090/svn/more_svn@184 f2e7b36f-b0e8-e445-a25c-5efde7ec30fc

--- Release branches/more_1.x/src/src/org/more/core/classcode/RootClassLoader.java
+++ Release branches/more_1.x/src/src/org/more/core/classcode/RootClassLoader.java
@@ -14,7 +14,10 @@
  * limitations under the License.
  */
 package org.more.core.classcode;
-import java.util.LinkedList;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 /**
  * {@link ClassEngine}引擎的类装载器，该类的职责是负责装载所有引擎生成的Class字节码。<br/>
  * 除此之外当引擎需要装载某些参数的类型时也需要通过该类装载器来装载。
@@ -22,38 +25,62 @@ import java.util.LinkedList;
  * @author 赵永春 (zyc@byshell.org)
  */
 public class RootClassLoader extends ClassLoader {
-    private LinkedList<ClassEngine> classEngineList = null;
+    private Map<String, ClassEngine> classMap2 = null;
+    private Map<String, ClassEngine> classMap  = null;
     /**创建引擎类装载器，parentClassLoader参数是该装载器所使用的父类装载器。*/
     public RootClassLoader(ClassLoader parentClassLoader) {
         super(parentClassLoader);
-        this.classEngineList = new LinkedList<ClassEngine>();
+        this.classMap = new HashMap<String, ClassEngine>();
+        this.classMap2 = new HashMap<String, ClassEngine>();
     }
     /**负责装载新类。*/
     protected final Class<?> findClass(String name) throws ClassNotFoundException {
-        for (ClassEngine engine : this.classEngineList)
-            if (engine.getClassName().equals(name) == true) {
-                byte[] bs = engine.toBytes();
-                return this.defineClass(name, bs, 0, bs.length);
-            }
+        if (this.classMap.containsKey(name) == true) {
+            byte[] bs = this.classMap.get(name).toBytes();
+            return this.defineClass(name, bs, 0, bs.length);
+        }
         return super.findClass(name);
     }
     /**获取某一个已经类型的字节码，该字节码必须是通过{@link ClassEngine}引擎生成的。*/
     public byte[] toBytes(Class<?> type) {
-        for (ClassEngine engine : this.classEngineList) {
-            Class<?> findType = engine.toClass();
-            if (findType == type)
-                return engine.toBytes();
-        }
+        ClassLoader cl = type.getClassLoader();
+        if (cl instanceof RootClassLoader)
+            return ((RootClassLoader) cl).getRegeditEngine(type.getName()).toBytes();
         return null;
     }
     /**注册一个{@link ClassEngine}引擎到类装载器中。*/
     public void regeditEngine(ClassEngine classEngine) {
-        if (this.classEngineList.contains(classEngine) == false)
-            this.classEngineList.add(classEngine);
+        String cn = classEngine.getClassName();
+        if (this.classMap.containsKey(cn) == false) {
+            this.classMap.put(cn, classEngine);
+            cn = "/" + cn.replace(".", "/") + ".class";
+            this.classMap2.put(cn, classEngine);
+        }
     }
     /**解除一个{@link ClassEngine}引擎的注册，接触注册之后该类装载器将不能再次获取到其字节码。*/
     public void unRegeditEngine(ClassEngine classEngine) {
-        if (this.classEngineList.contains(classEngine) == true)
-            this.classEngineList.remove(classEngine);
+        String cn = classEngine.getClassName();
+        if (this.classMap.containsKey(cn) == true) {
+            this.classMap.remove(cn);
+            cn = "/" + cn.replace(".", "/") + ".class";
+            this.classMap2.put(cn, classEngine);
+        }
+    }
+    /**获取一个以注册的{@link ClassEngine}引擎。*/
+    public ClassEngine getRegeditEngine(String className) {
+        if (this.classMap.containsKey(className) == true)
+            return this.classMap.get(className);
+        return null;
+    }
+    public InputStream getResourceAsStream(String name) {
+        if (this.classMap.containsKey(name) == true) {
+            ClassEngine ce = this.classMap.get(name);
+            return new ByteArrayInputStream(ce.toBytes());
+        }
+        if (this.classMap2.containsKey(name) == true) {
+            ClassEngine ce = this.classMap2.get(name);
+            return new ByteArrayInputStream(ce.toBytes());
+        }
+        return super.getResourceAsStream(name);
     }
 }
