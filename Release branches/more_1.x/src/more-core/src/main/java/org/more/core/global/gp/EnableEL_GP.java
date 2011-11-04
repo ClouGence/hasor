d7d7aec34d173157008ce2f3bff2553b9debda43
zyc <zyc@hasor.net>
2011-11-04 11:14:57 +0000

    1.递交global的内置属性类。
    git-svn-id: http://127.0.0.1:9090/svn/more_svn@293 f2e7b36f-b0e8-e445-a25c-5efde7ec30fc

---
+++ Release branches/more_1.x/src/more-core/src/main/java/org/more/core/global/gp/EnableEL_GP.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.more.core.global.gp;
+import org.more.core.error.SupportException;
+import org.more.core.global.Global;
+import org.more.core.global.GlobalProperty;
+import org.more.util.StringConvertUtil;
+/**
+ * _global.enableEL
+ * @version : 2011-9-30
+ * @author 赵永春 (zyc@byshell.org)
+ */
+public class EnableEL_GP implements GlobalProperty {
+    public Object getValue(Global global) {
+        String oriString = global.getOriginalString("_global.enableEL");
+        return StringConvertUtil.parseBoolean(oriString, true);
+    }
+    public void setValue(Object value, Global global) {
+        throw new SupportException("_global.enableEL，属性不支持写操作。");
+    }
+}
---
+++ Release branches/more_1.x/src/more-core/src/main/java/org/more/core/global/gp/GroupCount_GP.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.more.core.global.gp;
+import org.more.core.error.SupportException;
+import org.more.core.global.Global;
+import org.more.core.global.GlobalProperty;
+/**
+ * _global.groupSize
+ * @version : 2011-9-30
+ * @author 赵永春 (zyc@byshell.org)
+ */
+public class GroupCount_GP implements GlobalProperty {
+    public Object getValue(Global global) {
+        return global.getConfigGroupCount();
+    }
+    public void setValue(Object value, Global global) {
+        throw new SupportException("_global.groupSize，属性不支持写操作。");
+    }
+}
