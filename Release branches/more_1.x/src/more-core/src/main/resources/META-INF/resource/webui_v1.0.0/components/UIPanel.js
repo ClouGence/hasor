61be1b12baf1245a04f1e453c6e339ec0bd785db
zyc <zyc@hasor.net>
2012-11-09 13:06:35 +0000

    1.增加了若干对话框弹出方法。
    git-svn-id: http://127.0.0.1:9090/svn/more_svn@409 f2e7b36f-b0e8-e445-a25c-5efde7ec30fc

--- Release branches/more_1.x/src/more-core/src/main/resources/META-INF/resource/webui_v1.0.0/components/UIPanel.js
+++ Release branches/more_1.x/src/more-core/src/main/resources/META-INF/resource/webui_v1.0.0/components/UIPanel.js
@@ -15,15 +15,69 @@ WebUI.Component.$extends("UIPanel", "UIComponent", {
 				errorFun.call($this, res);
 		});
 	},
-	/** 载入预先定义的页面 */
-	loadPage : function(paramMap, callBackFun, errorFun) {
-		if (WebUI.isNaN(paramMap) == true)
-			paramMap = {};
-		this.load(this.pageURL(), paramMap, callBackFun, errorFun);
+	/** 第一个参数是固定值，第二个参数是默认值，第三个是用户的配置。 */
+	showDialog : function(fixedParamMap, defaultParamMap, dialogParamMap) {
+		/* url, paramMap, callBackFun, errorFun */
+		if (WebUI.isNaN(dialogParamMap) == true)
+			dialogParamMap = {};
+		// 固定参数
+		for ( var v in fixedParamMap)
+			dialogParamMap[v] = fixedParamMap[v];
+		dialogParamMap["id"] = this.clientID;
+		dialogParamMap["content"] = $(this.getElement()).html();
+		// 默认值
+		for ( var v in defaultParamMap)
+			if (WebUI.isNaN(dialogParamMap[v]) == true)
+				dialogParamMap[v] = defaultParamMap[v];
+		// dialog
+		$.dialog(dialogParamMap);
+	},
+	/** 将面板的内容作为对话框弹出（公告版）。 如下几个属性被锁定不可设置：id、content、fixed、drag、resize、max、min */
+	dialogNotice : function(dialogParamMap) {
+		// 固定参数
+		var fixedParamMap = {};
+		fixedParamMap["fixed"] = true;
+		fixedParamMap["drag"] = false;
+		fixedParamMap["resize"] = false;
+		fixedParamMap["max"] = false;
+		fixedParamMap["min"] = false;
+		fixedParamMap["lock"] = false;
+		// 默认配置
+		var defaultParamMap = {};
+		defaultParamMap["left"] = "100%";
+		defaultParamMap["top"] = "100%";
+		defaultParamMap["width"] = 200;
+		defaultParamMap["height"] = 100;
+		defaultParamMap["title"] = "公告";
+		// dialog
+		this.showDialog(fixedParamMap, defaultParamMap, dialogParamMap);
+	},
+	/** 将面板的内容作为对话框弹出（面板）。 如下几个属性被锁定不可设置：id、content、fixed、drag、resize、max、min */
+	dialogPanel : function(dialogParamMap) {
+		// 固定参数
+		var fixedParamMap = {};
+		fixedParamMap["fixed"] = true;
+		fixedParamMap["drag"] = false;
+		fixedParamMap["resize"] = false;
+		fixedParamMap["max"] = false;
+		fixedParamMap["min"] = false;
+		// 默认配置
+		var defaultParamMap = {};
+		defaultParamMap["lock"] = false;
+		defaultParamMap["left"] = "60%";
+		defaultParamMap["top"] = "60%";
+		defaultParamMap["width"] = "60%";
+		defaultParamMap["height"] = "60%";
+		defaultParamMap["title"] = "Panel";
+		// dialog
+		this.showDialog(fixedParamMap, defaultParamMap, dialogParamMap);
 	},
 	/** 构造方法 */
 	"<init>" : function() {
 		/** 要载入的页面（RW） */
 		this.defineProperty("pageURL", "RW");
+		var pageURL = this.pageURL();
+		if (WebUI.isNaN(pageURL) == false)
+			this.load(pageURL);
 	}
 });
