b13c26f0a0c493e11914f224aa2efc10bbfb66fb
zyc <zyc@hasor.net>
2013-09-26 16:54:03 +0800

    1.递交Restful相关的类代码。

---
+++ src/hasor-web/src/main/java/net/hasor/web/restful/ActionDefine.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2008-2009 the original 赵永春(zyc@hasor.net).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.web.restful;
+import java.lang.reflect.Method;
+/**
+ * Action定义
+ * @version : 2013-8-12
+ * @author 赵永春 (zyc@hasor.net)
+ */
+public interface ActionDefine {
+    /**在调用Action之前引发的事件*/
+    public static String Event_BeforeInvoke = "ActionInvoke_Event_BeforeInvoke";
+    /**在调用Action之后引发的事件*/
+    public static String Event_AfterInvoke  = "ActionInvoke_Event_AfterInvoke";
+    //
+    /**获取Action可以接收的方法*/
+    public String[] getHttpMethod();
+    /**获取目标方法。*/
+    public Method getTargetMethod();
+}
---
+++ src/hasor-web/src/main/java/net/hasor/web/restful/Get.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2008-2009 the original ’‘”¿¥∫(zyc@hasor.net).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.web.restful;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+/**
+ * Indicates that the annotated method responds to HTTP GET requests
+ * @see HttpMethod
+ * @version : 2013-3-26
+ * @author ’‘”¿¥∫ (zyc@hasor.net)
+ */
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod(HttpMethod.GET)
+public @interface Get {}
