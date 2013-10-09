4aa5c2b31c670687c542c6582ec0390796867150
zyc <zyc@hasor.net>
2013-10-09 17:58:12 +0800

    1.递交三个结果集处理接口。

---
+++ src/hasor-jdbc/src/main/java/net/hasor/jdbc/jdbc/RowMapper.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.jdbc.jdbc;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ * 这个接口用来映射 JDBC 结果集中一行数据。
+ * @version : 2013-10-9
+ * @author 赵永春(zyc@hasor.net)
+ */
+public interface RowMapper<T> {
+    /**实现这个方法为结果集的一行记录进行转换，并将最终转换结果返回。
+     * 如果返回为 null 等同于忽略该行。需要注意，不要调用结果集的 next() 方法。*/
+    public T mapRow(ResultSet rs, int rowNum) throws SQLException;
+}
