f0720dba0008d17917979601b989962f3ad1637f
zyc <zyc@hasor.net>
2014-12-21 13:40:31 +0800

    1.针对接口层新增的适配器层。适配器层面向于实现细节。

---
+++ hasor-rsf/src/main/java/net/hasor/rsf/adapter/AbstractRsfContext.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2008-2009 the original 赵永春(zyc@hasor.net).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.rsf.adapter;
+import java.util.concurrent.Executor;
+import net.hasor.rsf.RsfContext;
+import net.hasor.rsf.serialize.SerializeFactory;
+/**
+ * 
+ * @version : 2014年11月12日
+ * @author 赵永春(zyc@hasor.net)
+ */
+public abstract class AbstractRsfContext implements RsfContext {
+    /**获取{@link Executor}用于安排执行任务。*/
+    public abstract Executor getCallExecute(String serviceName);
+    /**获取序列化管理器。*/
+    public abstract SerializeFactory getSerializeFactory();
+    /**获取注册中心。*/
+    public abstract AbstractBindCenter getBindCenter();
+}
