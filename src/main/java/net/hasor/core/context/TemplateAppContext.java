ba9ee75c8e2b4702e957efde34726b43d596a73a
zyc <zyc@hasor.net>
2015-12-28 22:04:38 +0800

    1.modules 的 loadModule 配置项，准备从全局生效改为局部生效。

--- src/main/java/net/hasor/core/context/TemplateAppContext.java
+++ src/main/java/net/hasor/core/context/TemplateAppContext.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import org.more.convert.ConverterUtils;
 import org.more.util.ArrayUtils;
 import org.more.util.ClassUtils;
 import org.more.util.StringUtils;
@@ -277,20 +278,22 @@ public abstract class TemplateAppContext<C extends BeanContainer> implements App
     protected Module[] findModules() {
         ArrayList<String> moduleTyleList = new ArrayList<String>();
         Environment env = this.getEnvironment();
-        boolean loadModule = env.getSettings().getBoolean("hasor.modules.loadModule", true);
-        if (loadModule) {
-            List<XmlNode> allModules = env.getSettings().merageXmlNode("hasor.modules", "module");
-            for (XmlNode module : allModules) {
-                if (module ==null ){
-                    continue;
-                }
-                String moduleTypeString = module.getText();
-                if (StringUtils.isBlank(moduleTypeString)) {
-                    continue;
-                }
-                if (!moduleTyleList.contains(moduleTypeString)) {
-                    moduleTyleList.add(moduleTypeString);
-                }
+        List<XmlNode> allModules = env.getSettings().merageXmlNode("hasor.modules", "module");
+        for (XmlNode module : allModules) {
+            if (module == null) {
+                continue;
+            }
+            String loadModuleStr = module.getAttribute("loadModule");
+            boolean loadModuleBool = (Boolean) ConverterUtils.convert(loadModuleStr, Boolean.TYPE);
+            if (loadModuleBool == false) {
+                continue;ss
+            }
+            String moduleTypeString = module.getText();
+            if (StringUtils.isBlank(moduleTypeString)) {
+                continue;
+            }
+            if (!moduleTyleList.contains(moduleTypeString)) {
+                moduleTyleList.add(moduleTypeString);
             }
         }
         //
