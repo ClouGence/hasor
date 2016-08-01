0341cb3428a0a883fdd221a78ae4b0ba40be2b6b
zyc <zyc@hasor.net>
2016-08-01 14:35:43 +0800

    1.更新下载链接。

--- exampleWeb/src/main/resources/META-INF/init_sql_version_info.sql
+++ exampleWeb/src/main/resources/META-INF/init_sql_version_info.sql
@@ -1,130 +1,130 @@
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.4.1','2016-08-01',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.4.1',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.4.1.zip',
 	'http://project.hasor.net/apis/2.4.1/index.html',
 	'http://project.hasor.net/apis/download/2.4.1.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.4.0','2016-07-29',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.4.0',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.4.0.zip',
 	'http://project.hasor.net/apis/2.4.0/index.html',
 	'http://project.hasor.net/apis/download/2.4.0.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.3.3','2016-06-16',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.3.3',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.3.3.zip',
 	'http://project.hasor.net/apis/2.3.3/index.html',
 	'http://project.hasor.net/apis/download/2.3.3.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.3.2','2016-05-29',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.3.2',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.3.2.zip',
 	'http://project.hasor.net/apis/2.3.2/index.html',
 	'http://project.hasor.net/apis/download/2.3.2.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.3.1','2016-04-13',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.3.1',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.3.1.zip',
 	'http://project.hasor.net/apis/2.3.1/index.html',
 	'http://project.hasor.net/apis/download/2.3.1.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.3.0','2016-04-06',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.3.0',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.3.0.zip',
 	'http://project.hasor.net/apis/2.3.0/index.html',
 	'http://project.hasor.net/apis/download/2.3.0.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.2.0','2016-02-23',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.2.0',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.2.0.zip',
 	'http://project.hasor.net/apis/2.2.0/index.html',
 	'http://project.hasor.net/apis/download/2.2.0.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.1.0','2016-01-17',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.1.0',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.1.0.zip',
 	'http://project.hasor.net/apis/2.1.0/index.html',
 	'http://project.hasor.net/apis/download/2.1.0.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('2.0.0','2015-11-27',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-2.0.0',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-2.0.0.zip',
 	'http://project.hasor.net/apis/2.0.0/index.html',
 	'http://project.hasor.net/apis/download/2.0.0.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('1.2.0','2015-08-20',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-1.2.0',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-1.2.0.zip',
 	'http://project.hasor.net/apis/1.2.0/index.html',
 	'http://project.hasor.net/apis/download/1.2.0.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('1.1.0','2015-07-09',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-1.1.0',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-1.1.0.zip',
 	'http://project.hasor.net/apis/1.1.0/index.html',
 	'http://project.hasor.net/apis/download/1.1.0.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('1.0.0','2015-07-03',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-1.0.0',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-1.0.0.zip',
 	'http://project.hasor.net/apis/1.0.0/index.html',
 	'http://project.hasor.net/apis/download/1.0.0.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.14','2015-06-18',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.14',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.14.zip',
 	'http://project.hasor.net/apis/0.0.14/index.html',
 	'http://project.hasor.net/apis/download/0.0.14.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.13','2015-05-09',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.13',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.13.zip',
 	'http://project.hasor.net/apis/0.0.13/index.html',
 	'http://project.hasor.net/apis/download/0.0.13.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.12','2015-05-09',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.12',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.12.zip',
 	'http://project.hasor.net/apis/0.0.12/index.html',
 	'http://project.hasor.net/apis/download/0.0.12.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.11','2015-04-24',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.11',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.11.zip',
 	'http://project.hasor.net/apis/0.0.11/index.html',
 	'http://project.hasor.net/apis/download/0.0.11.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.10','2015-04-03',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.10',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.10.zip',
 	'http://project.hasor.net/apis/0.0.10/index.html',
 	'http://project.hasor.net/apis/download/0.0.10.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.9','2014-09-21',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.9',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.9.zip',
 	'http://project.hasor.net/apis/0.0.9/index.html',
 	'http://project.hasor.net/apis/download/0.0.9.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.8','2014-07-26',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.8',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.8.zip',
 	'http://project.hasor.net/apis/0.0.8/index.html',
 	'http://project.hasor.net/apis/download/0.0.8.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.7','2014-01-17',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.7',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.7.zip',
 	'http://project.hasor.net/apis/0.0.7/index.html',
 	'http://project.hasor.net/apis/download/0.0.7.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.6','2013-12-21',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.6',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.6.zip',
 	'http://project.hasor.net/apis/0.0.6/index.html',
 	'http://project.hasor.net/apis/download/0.0.6.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.5','2013-11-25',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.5',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.5.zip',
 	'http://project.hasor.net/apis/0.0.5/index.html',
 	'http://project.hasor.net/apis/download/0.0.5.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.4','2013-11-11',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.4',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.4.zip',
 	'http://project.hasor.net/apis/0.0.4/index.html',
 	'http://project.hasor.net/apis/download/0.0.4.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.3','2013-10-09',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.3',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.3.zip',
 	'http://project.hasor.net/apis/0.0.3/index.html',
 	'http://project.hasor.net/apis/download/0.0.3.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.2','2013-09-29',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.2',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.2.zip',
 	'http://project.hasor.net/apis/0.0.2/index.html',
 	'http://project.hasor.net/apis/download/0.0.2.zip');
 insert into VersionInfo(version,releaseDate,downloadURL,apiURL,downloadApiURL)
 	values('0.0.1','2013-09-15',
-	'http://git.oschina.net/zycgit/hasor/repository/archive/Release.Hasor-0.0.1',
+	'https://github.com/zycgit/hasor/archive/Release.Hasor-0.0.1.zip',
 	'http://project.hasor.net/apis/0.0.1/index.html',
 	'http://project.hasor.net/apis/download/0.0.1.zip');
