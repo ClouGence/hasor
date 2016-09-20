f1ab81393b9b9d4d744169c8a6e68fecee1ad0d1
zyc <zyc@hasor.net>
2016-09-20 16:43:22 +0800

    1.更新注释信息。

--- rsf-center/rsf-center-runtime/src/main/java/net/hasor/rsf/center/server/DataAdapter.java
+++ rsf-center/rsf-center-runtime/src/main/java/net/hasor/rsf/center/server/DataAdapter.java
@@ -24,21 +24,46 @@ import java.util.List;
  * @author 赵永春(zyc@hasor.net)
  */
 public interface DataAdapter {
-    /** 查询对象 */
+    /**
+     * 查询对象
+     *  <i>核心方法,负责数据查询。</i>
+     */
     public Result<ObjectDO> queryObjectByID(String objectID);
 
-    /** 删除对象 */
-    public Result<Boolean> removeObjectByID(String objectID);
-
-    /** 保存对象,并将改对象关联到refObjectID对象上 */
+    /**
+     * 保存对象,并将改对象关联到refObjectID对象上 
+     *  <i>核心方法,负责数据存储。</i>
+     */
     public Result<Boolean> storeObject(ObjectDO object);
 
-    /** 检索关联的对象列表 */
+    /**
+     * 检索关联的对象列表
+     *  <i>核心方法,负责数据查询。</i>
+     *  <i>必须支持: 负责查询服务下的 (提供者 or 消费者) 列表,以及服务路由等信息。</i>
+     *  <i>option参数是可选支持: 负责限定查询条件,因为服务提供者和服务消费者都会挂在 refObjectID 下面,因此实现该参数会有助于减轻Center的负担。</i>
+     * @param refObjectID 关联的服务ID
+     * @param option 查询过滤参数(可选)
+     */
     public Result<List<ObjectDO>> queryObjectListByID(String refObjectID, QueryOption option);
 
-    /** 清空对象上的所有关联 */
-    public Result<Boolean> clearRef(String objectID);
-
-    /** 刷新对象的时间(可选支持,如果不支持,center将会采取storeObject方法重新保存) */
+    /**
+     * 刷新对象的时间
+     *  <i>RSF服务心跳依赖这个方法。</i>
+     *  <i>可选支持: 如果不支持,RSF客户端会在每次心跳时进行服务的重新注册。</i>
+     */
     public Result<Boolean> refreshObject(String objectID);
+
+    /**
+     * 删除对象
+     *  <i>服务下线需要实现这个接口。</i>
+     *  <i>可选支持: 如果不支持,Center在每次推送地址的时候会推送失效的地址。虽然RSF客户端有地址有效性校验机制,但是这会加重运行负担。</i>
+     */
+    public Result<Boolean> removeObjectByID(String objectID);
+
+    /**
+     *  清空对象上的所有关联
+     *  <i>Center定时扫描服务进行数据清理需要实现这个接口。</i>
+     *  <i>可选支持: Center会定时的检查服务数据健康度,并做相应的清理工作。如果不实现该方法,在每次推送地址的时候会推送失效的地址。虽然RSF客户端有地址有效性校验机制,但是这会加重运行负担。</i>
+     */
+    public Result<Boolean> clearRef(String objectID);
 }
