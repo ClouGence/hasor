caf5349e9b87710dd7c2efbee31f7ff90013e2c7
zyc <zyc@hasor.net>
2017-07-18 23:21:39 +0800

    1.完成 ASA、ASM 两个指令。

--- hasor-dataql/src/main/java/net/hasor/dataql/runtime/process/ASA.java
+++ hasor-dataql/src/main/java/net/hasor/dataql/runtime/process/ASA.java
@@ -1,12 +1,15 @@
 package net.hasor.dataql.runtime.process;
 import net.hasor.core.utils.StringUtils;
 import net.hasor.dataql.domain.inst.Instruction;
-import net.hasor.dataql.result.ListModel;
-import net.hasor.dataql.runtime.InsetProcess;
-import net.hasor.dataql.runtime.ProcessContet;
-import net.hasor.dataql.runtime.ProcessException;
+import net.hasor.dataql.result.ObjectModel;
+import net.hasor.dataql.runtime.*;
 import net.hasor.dataql.runtime.struts.ListResultStruts;
 import net.hasor.dataql.runtime.struts.MemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Created by yongchun.zyc on 2017/7/13.
  */
@@ -16,23 +19,76 @@ class ASA implements InsetProcess {
         return ASA;
     }
     @Override
-    public void doWork(Instruction inst, MemStack memStack, ProcessContet context) throws ProcessException {
-        String typeString = inst.getString(0);
+    public void doWork(InstSequence sequence, MemStack memStack, ProcessContet context) throws ProcessException {
+        //
+        // .读取返回值并包装成 ResultStruts
+        String typeString = sequence.currentInst().getString(0);
         Class<?> objectType = null;
         if (StringUtils.isNotBlank(typeString)) {
             objectType = context.loadType(typeString);
         } else {
-            objectType = ListModel.class;
+            objectType = ObjectModel.class;
         }
-        //
         Object toType = null;
         try {
             toType = objectType.newInstance();
         } catch (Exception e) {
             throw new ProcessException("ASA -> " + e.getMessage(), e);
         }
-        //
         Object result = memStack.pop();
-        memStack.push(new ListResultStruts(result, toType));
+        memStack.push(new ListResultStruts(toType));
+        //
+        // .圈定处理结果集的指令集
+        final AtomicInteger dogs = new AtomicInteger(0);
+        InstSequence subSequence = sequence.findSubSequence(new InstFilter() {
+            public boolean isExit(Instruction inst) {
+                //
+                if (ASM == inst.getInstCode() || ASA == inst.getInstCode()) {
+                    dogs.incrementAndGet();
+                    return false;
+                }
+                //
+                if (ASE == inst.getInstCode()) {
+                    dogs.decrementAndGet();
+                    if (dogs.get() == 0) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        //
+        // .对结果集进行迭代处理
+        Collection<Object> dataSet = toCollection(result);
+        for (Object obj : dataSet) {
+            subSequence.reset();                        // 重置执行序列
+            memStack.pushData(obj);                     // 设置DS
+            context.processInset(subSequence, memStack);// 执行序列
+            memStack.popData();                         // 销毁DS
+        }
+        //
+        // .处理完毕跳到出口
+        sequence.jumpTo(subSequence.exitPosition() - 1);
+    }
+    private Collection<Object> toCollection(Object curData) {
+        Collection<Object> listData = null;
+        if (curData == null) {
+            listData = new ArrayList<Object>();
+        } else {
+            if (!(curData instanceof Collection)) {
+                if (curData.getClass().isArray()) {
+                    listData = new ArrayList<Object>();
+                    for (Object obj : (Object[]) curData) {
+                        listData.add(obj);
+                    }
+                } else {
+                    listData = Arrays.asList(curData);
+                }
+            } else {
+                listData = (Collection<Object>) curData;
+            }
+        }
+        //
+        return listData;
     }
 }
--- hasor-dataql/src/main/java/net/hasor/dataql/runtime/process/ASM.java
+++ hasor-dataql/src/main/java/net/hasor/dataql/runtime/process/ASM.java
@@ -2,11 +2,11 @@ package net.hasor.dataql.runtime.process;
 import net.hasor.core.utils.StringUtils;
 import net.hasor.dataql.domain.inst.Instruction;
 import net.hasor.dataql.result.ObjectModel;
-import net.hasor.dataql.runtime.InsetProcess;
-import net.hasor.dataql.runtime.ProcessContet;
-import net.hasor.dataql.runtime.ProcessException;
+import net.hasor.dataql.runtime.*;
 import net.hasor.dataql.runtime.struts.MemStack;
 import net.hasor.dataql.runtime.struts.ObjectResultStruts;
+
+import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Created by yongchun.zyc on 2017/7/13.
  */
@@ -16,23 +16,52 @@ class ASM implements InsetProcess {
         return ASM;
     }
     @Override
-    public void doWork(Instruction inst, MemStack memStack, ProcessContet context) throws ProcessException {
-        String typeString = inst.getString(0);
+    public void doWork(InstSequence sequence, MemStack memStack, ProcessContet context) throws ProcessException {
+        //
+        // .读取返回值并包装成 ResultStruts
+        String typeString = sequence.currentInst().getString(0);
         Class<?> objectType = null;
         if (StringUtils.isNotBlank(typeString)) {
             objectType = context.loadType(typeString);
         } else {
             objectType = ObjectModel.class;
         }
-        //
         Object toType = null;
         try {
             toType = objectType.newInstance();
         } catch (Exception e) {
             throw new ProcessException("ASM -> " + e.getMessage(), e);
         }
-        //
         Object result = memStack.pop();
-        memStack.push(new ObjectResultStruts(result, toType));
+        memStack.push(new ObjectResultStruts(toType));
+        //
+        // .圈定处理结果集的指令集
+        final AtomicInteger dogs = new AtomicInteger(0);
+        InstSequence subSequence = sequence.findSubSequence(new InstFilter() {
+            public boolean isExit(Instruction inst) {
+                //
+                if (ASM == inst.getInstCode() || ASA == inst.getInstCode()) {
+                    dogs.incrementAndGet();
+                    return false;
+                }
+                //
+                if (ASE == inst.getInstCode()) {
+                    dogs.decrementAndGet();
+                    if (dogs.get() == 0) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        //
+        // .对结果集进行迭代处理
+        subSequence.reset();                        // 重置执行序列
+        memStack.pushData(result);                  // 设置DS
+        context.processInset(subSequence, memStack);// 执行序列
+        memStack.popData();                         // 销毁DS
+        //
+        // .处理完毕跳到出口
+        sequence.jumpTo(subSequence.exitPosition() - 1);
     }
 }
