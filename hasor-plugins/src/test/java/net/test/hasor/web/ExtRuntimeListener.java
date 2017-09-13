e970c6d81ed7e0796bb992f98d02d858eea48fb8
zyc <zyc@hasor.net>
2017-09-13 18:26:51 +0800

    1.格式错误，修复。

--- hasor-plugins/src/test/java/net/test/hasor/web/ExtRuntimeListener.java
+++ hasor-plugins/src/test/java/net/test/hasor/web/ExtRuntimeListener.java
@@ -1,5 +1,6 @@
 package net.test.hasor.web;
 import net.hasor.core.Hasor;
+import net.hasor.web.mime.MimeTypeSupplier;
 import net.hasor.web.startup.RuntimeListener;
 import org.junit.Test;
 
@@ -15,29 +16,19 @@ public class ExtRuntimeListener extends RuntimeListener {
         return hasor;
     }
     /**
-     * Created by yongchun.zyc on 2017/2/25.
+     * S
+     * @version : 2016年2月15日
+     * @author 赵永春(zyc@hasor.net)
      */
-    public static class ExtRuntimeListener extends RuntimeListener {
-        protected Hasor createAppContext(ServletContext sc) throws Throwable {
-            Hasor hasor = super.createAppContext(sc);
-            hasor.setMainSettings(".....");
-            return hasor;
-        }
-        /**
-         * S
-         * @version : 2016年2月15日
-         * @author 赵永春(zyc@hasor.net)
-         */
-        public static class MimeTypeTest {
-            @Test
-            public void mimeTypeTest() throws IOException {
-                MimeTypeSupplier mimeTypeContext = new MimeTypeSupplier(null);
-                mimeTypeContext.loadStream("/META-INF/mime.types.xml");
-                mimeTypeContext.loadStream("mime.types.xml");
-                //
-                String htmlType = mimeTypeContext.get("html");
-                assert "text/html".equalsIgnoreCase(htmlType);
-            }
+    public static class MimeTypeTest {
+        @Test
+        public void mimeTypeTest() throws IOException {
+            MimeTypeSupplier mimeTypeContext = new MimeTypeSupplier(null);
+            mimeTypeContext.loadStream("/META-INF/mime.types.xml");
+            mimeTypeContext.loadStream("mime.types.xml");
+            //
+            String htmlType = mimeTypeContext.get("html");
+            assert "text/html".equalsIgnoreCase(htmlType);
         }
     }
 }
