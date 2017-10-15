e0feef1b0bcc3175b129998ae6cd32423130de40
zyc <zyc@hasor.net>
2017-10-15 18:25:33 +0800

    1.发布脚本。

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # ----------------------------------------------------------------------------
-mvn clean && cd core && mvn clean deploy $@ && cd ../
-mvn clean && cd framework && mvn clean deploy $@ && cd ../
-mvn clean && cd registry  && mvn clean deploy $@ && cd ../
+mvn clean install
+mvn clean && cd hasor-commons  && mvn clean deploy $@ && cd ../
+mvn clean && cd hasor-core     && mvn clean deploy $@ && cd ../
+mvn clean && cd hasor-db       && mvn clean deploy $@ && cd ../
+mvn clean && cd hasor-web      && mvn clean deploy $@ && cd ../
+mvn clean && cd hasor-dataql   && mvn clean deploy $@ && cd ../
+mvn clean && cd hasor-rsf      && mvn clean deploy $@ && cd ../
+mvn clean && cd hasor-registry && mvn clean deploy $@ && cd ../
+mvn clean && cd hasor-plugins  && mvn clean deploy $@ && cd ../
 mvn clean && mvn package $@
