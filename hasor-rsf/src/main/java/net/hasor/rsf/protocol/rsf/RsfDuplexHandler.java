c65c8a16ac5ccdee39e0c98146e10e94ab890b8d
zyc <zyc@hasor.net>
2017-11-20 16:47:30 +0800

    1.RsfDuplexHandler类换个位置，降级为 netty专用。

--- hasor-rsf/src/main/java/net/hasor/rsf/protocol/rsf/RsfDuplexHandler.java
+++ hasor-rsf/src/main/java/net/hasor/rsf/protocol/rsf/RsfDuplexHandler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.hasor.rsf.rpc.net;
+package net.hasor.rsf.protocol.rsf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelInboundHandler;
 import io.netty.channel.ChannelOutboundHandler;
