2f5ff4e293ec356b602171aa0323a21c2ef7d07f
zyc <zyc@hasor.net>
2017-11-24 10:57:19 +0800

    1.HttpRequest HttpResponse 接口确定下来了。

---
+++ hasor-rsf/src/main/java/net/hasor/rsf/rpc/net/http/RsfHttpRequest.java
@@ -0,0 +1,316 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.rsf.rpc.net.http;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+/**
+ * Http Request 删减版
+ * @version : 2017年11月22日
+ * @author 赵永春(zyc@hasor.net)
+ */
+public interface RsfHttpRequest {
+    /**
+     * Returns the part of this request's URL from the protocol
+     * name up to the query string in the first line of the HTTP request.
+     * The web container does not decode this String.
+     * For example:
+     *
+     * <table summary="Examples of Returned Values">
+     * <tr align=left><th>First line of HTTP request      </th>
+     * <th>     Returned Value</th>
+     * <tr><td>POST /some/path.html HTTP/1.1<td><td>/some/path.html
+     * <tr><td>GET http://foo.bar/a.html HTTP/1.0
+     * <td><td>/a.html
+     * <tr><td>HEAD /xyz?a=b HTTP/1.1<td><td>/xyz
+     * </table>
+     *
+     * <p>To reconstruct an URL with a scheme and host, use
+     *
+     * @return a <code>String</code> containing the part of the URL from the protocol name up to the query string
+     *
+     */
+    public String getRequestURI();
+
+    /**
+     * Returns the query string that is contained in the request
+     * URL after the path. This method returns <code>null</code>
+     * if the URL does not have a query string. Same as the value
+     * of the CGI variable QUERY_STRING.
+     *
+     * @return a <code>String</code> containing the query
+     *			string or <code>null</code> if the URL
+     *			contains no query string. The value is not
+     *			decoded by the container.
+     */
+    public String getQueryString();
+
+    /**
+     * Returns the portion of the request URI that indicates the context
+     * of the request. The context path always comes first in a request
+     * URI. The path starts with a "/" character but does not end with a "/"
+     * character. For servlets in the default (root) context, this method
+     * returns "". The container does not decode this string.
+     *
+     * @return a <code>String</code> specifying the portion of the request URI that indicates the context of the request
+     */
+    public String getContextPath();
+
+    /**
+     * Returns the name of the HTTP method with which this
+     * request was made, for example, GET, POST, or PUT.
+     * Same as the value of the CGI variable REQUEST_METHOD.
+     *
+     * @return a <code>String</code> specifying the name of the method with which this request was made
+     */
+    public String getMethod();
+
+    /**
+     * Returns the value of the named attribute as an <code>Object</code>,
+     * or <code>null</code> if no attribute of the given name exists.
+     * @param name a <code>String</code> specifying the name of the attribute
+     * @return an <code>Object</code> containing the value of the attribute, or <code>null</code> if the attribute does not exist
+     */
+    public Object getAttribute(String name);
+
+    /**
+     * Returns an <code>Enumeration</code> containing the
+     * names of the attributes available to this request.
+     * This method returns an empty <code>Enumeration</code>
+     * if the request has no attributes available to it.
+     *
+     * @return an <code>Enumeration</code> of strings containing the names of the request's attributes
+     */
+    public Enumeration<String> getAttributeNames();
+
+    /**
+     * Stores an attribute in this request.
+     * Attributes are reset between requests.
+     * @param name a <code>String</code> specifying the name of the attribute
+     * @param o the <code>Object</code> to be stored
+     */
+    public void setAttribute(String name, Object o);
+
+    /**
+     * Retrieves the body of the request as binary data using a {@link InputStream}.
+     * @return a {@link InputStream} object containing the body of the request
+     * @exception IOException if an input or output exception occurred
+     */
+    public InputStream getInputStream() throws IOException;
+
+    /**
+     *
+     * Removes an attribute from this request.  This method is not
+     * generally needed as attributes only persist as long as the request is being handled.
+     * @param name a <code>String</code> specifying the name of the attribute to remove
+     */
+    public void removeAttribute(String name);
+
+    /**
+     * Returns the name of the character encoding used in the body of this
+     * request. This method returns <code>null</code> if the request
+     * does not specify a character encoding
+     * @return a <code>String</code> containing the name of the character encoding, or <code>null</code> if the request does not specify a character encoding
+     */
+    public String getCharacterEncoding();
+
+    /**
+     * Overrides the name of the character encoding used in the body of this
+     * request. This method must be called prior to reading request parameters
+     * or reading input using getReader(). Otherwise, it has no effect.
+     *
+     * @param env <code>String</code> containing the name of the character encoding.
+     * @throws UnsupportedEncodingException if this ServletRequest is still
+     * in a state where a character encoding may be set, but the specified encoding is invalid
+     */
+    public void setCharacterEncoding(String env) throws UnsupportedEncodingException;
+
+    /**
+     * Returns the length, in bytes, of the request body and made available by the input stream, or -1
+     * if the length is not known ir is greater than Integer.MAX_VALUE. For HTTP servlets,
+     * same as the value of the CGI variable CONTENT_LENGTH.
+     *
+     * @return an integer containing the length of the request body or -1 if the length is not known or is greater than Integer.MAX_VALUE.
+     */
+    public long getContentLength();
+
+    /**
+     * Returns the MIME type of the body of the request, or <code>null</code> if the type is not known.
+     * For HTTP servlets, same as the value of the CGI variable CONTENT_TYPE.
+     *
+     * @return a <code>String</code> containing the name of the MIME type of the request, or null if the type is not known
+     */
+    public String getContentType();
+
+    /**
+     * Returns the value of the specified request header
+     * as a <code>String</code>. If the request did not include a header
+     * of the specified name, this method returns <code>null</code>.
+     * If there are multiple headers with the same name, this method
+     * returns the first head in the request.
+     * The header name is case insensitive. You can use
+     * this method with any request header.
+     *
+     * @param name a <code>String</code> specifying the header name
+     * @return a <code>String</code> containing the
+     *				value of the requested
+     *				header, or <code>null</code>
+     *				if the request does not
+     *				have a header of that name
+     */
+    public String getHeader(String name);
+
+    /**
+     * Returns all the values of the specified request header
+     * as an <code>Enumeration</code> of <code>String</code> objects.
+     *
+     * <p>Some headers, such as <code>Accept-Language</code> can be sent
+     * by clients as several headers each with a different value rather than
+     * sending the header as a comma separated list.
+     *
+     * <p>If the request did not include any headers
+     * of the specified name, this method returns an empty
+     * <code>Enumeration</code>.
+     * The header name is case insensitive. You can use
+     * this method with any request header.
+     *
+     * @param name a <code>String</code> specifying the header name
+     * @return an <code>Enumeration</code> containing
+     *                  	the values of the requested header. If
+     *                  	the request does not have any headers of
+     *                  	that name return an empty
+     *                  	enumeration. If
+     *                  	the container does not allow access to
+     *                  	header information, return null
+     */
+    public Enumeration<String> getHeaders(String name);
+
+    /**
+     * Returns an enumeration of all the header names
+     * this request contains. If the request has no
+     * headers, this method returns an empty enumeration.
+     *
+     * <p>Some servlet containers do not allow
+     * servlets to access headers using this method, in
+     * which case this method returns <code>null</code>
+     *
+     * @return an enumeration of all the
+     *				header names sent with this
+     *				request; if the request has
+     *				no headers, an empty enumeration;
+     *				if the servlet container does not
+     *				allow servlets to use this method,
+     *				<code>null</code>
+     */
+    public Enumeration<String> getHeaderNames();
+
+    /**
+     * Returns the value of a request parameter as a <code>String</code>,
+     * or <code>null</code> if the parameter does not exist. Request parameters
+     * are extra information sent with the request.  For HTTP servlets,
+     * parameters are contained in the query string or posted form data.
+     *
+     * <p>You should only use this method when you are sure the
+     * parameter has only one value. If the parameter might have
+     * more than one value, use {@link #getParameterValues}.
+     *
+     * <p>If you use this method with a multivalued
+     * parameter, the value returned is equal to the first value
+     * in the array returned by <code>getParameterValues</code>.
+     *
+     * <p>If the parameter data was sent in the request body, such as occurs with an HTTP POST request,
+     * then reading the body directly via {@link #getInputStream} can interfere with the execution of this method.
+     *
+     * @param name a <code>String</code> specifying the name of the parameter
+     * @return a <code>String</code> representing the single value of the parameter
+     * @see #getParameterValues
+     */
+    public String getParameter(String name);
+
+    /**
+     *
+     * Returns an <code>Enumeration</code> of <code>String</code>
+     * objects containing the names of the parameters contained
+     * in this request. If the request has
+     * no parameters, the method returns an empty <code>Enumeration</code>.
+     *
+     * @return an <code>Enumeration</code> of <code>String</code>
+     * objects, each <code>String</code> containing the name of
+     * a request parameter; or an empty <code>Enumeration</code>
+     * if the request has no parameters
+     */
+    public Enumeration<String> getParameterNames();
+
+    /**
+     * Returns an array of <code>String</code> objects containing
+     * all of the values the given request parameter has, or
+     * <code>null</code> if the parameter does not exist.
+     *
+     * <p>If the parameter has a single value, the array has a length of 1.
+     *
+     * @param name a <code>String</code> containing the name of the parameter whose value is requested
+     * @return an array of <code>String</code> objects containing the parameter's values
+     * @see #getParameter
+     */
+    public String[] getParameterValues(String name);
+
+    /**
+     * Returns the name and version of the protocol the request uses
+     * in the form <i>protocol/majorVersion.minorVersion</i>, for
+     * example, HTTP/1.1. For HTTP servlets, the value
+     * returned is the same as the value of the CGI variable
+     * <code>SERVER_PROTOCOL</code>.
+     *
+     * @return a <code>String</code> containing the protocol name and version number
+     */
+    public String getProtocol();
+
+    /**
+     * Returns the name of the scheme used to make this request, for example,
+     * <code>http</code>, <code>https</code>, or <code>ftp</code>.
+     * Different schemes have different rules for constructing URLs,
+     * as noted in RFC 1738.
+     *
+     * @return a <code>String</code> containing the name of the scheme used to make this request
+     */
+    public String getScheme();
+
+    /**
+     * Returns the Internet Protocol (IP) address of the client or last proxy that sent the request.
+     * For HTTP servlets, same as the value of the CGI variable <code>REMOTE_ADDR</code>.
+     * @return a <code>String</code> containing the IP address of the client that sent the request
+     */
+    public String getRemoteAddr();
+
+    /**
+     * Returns the Internet Protocol (IP) source port of the client or last proxy that sent the request.
+     * @return an integer specifying the port number
+     */
+    public int getRemotePort();
+
+    /**
+     * Returns the Internet Protocol (IP) address of the interface on which the request  was received.
+     * @return a <code>String</code> containing the IP address on which the request was received.
+     */
+    public String getLocalAddr();
+
+    /**
+     * Returns the Internet Protocol (IP) port number of the interface on which the request was received.
+     * @return an integer specifying the port number
+     */
+    public int getLocalPort();
+}
---
+++ hasor-rsf/src/main/java/net/hasor/rsf/rpc/net/http/RsfHttpResponse.java
@@ -0,0 +1,285 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.rsf.rpc.net.http;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collection;
+/**
+ * Http Response 删减版
+ * @version : 2017年11月22日
+ * @author 赵永春(zyc@hasor.net)
+ */
+public interface RsfHttpResponse {
+    /**
+     * Returns the name of the character encoding (MIME charset)
+     * used for the body sent in this response.
+     * The character encoding may have been specified explicitly
+     * using the {@link #setCharacterEncoding} or
+     * {@link #setContentType} methods. Explicit specifications take
+     * precedence over implicit specifications. Calls made
+     * to these methods after <code>getWriter</code> has been
+     * called or after the response has been committed have no
+     * effect on the character encoding. If no character encoding
+     * has been specified, <code>ISO-8859-1</code> is returned.
+     * <p>See RFC 2047 (http://www.ietf.org/rfc/rfc2047.txt)
+     * for more information about character encoding and MIME.
+     *
+     * @return a <code>String</code> specifying the name of
+     * the character encoding, for example, <code>UTF-8</code>
+     */
+    public String getCharacterEncoding();
+
+    /**
+     * Sets the character encoding (MIME charset) of the response
+     * being sent to the client, for example, to UTF-8.
+     * If the character encoding has already been set by
+     * {@link #setContentType}, this method overrides it.
+     * Calling {@link #setContentType} with the <code>String</code>
+     * of <code>text/html</code> and calling
+     * this method with the <code>String</code> of <code>UTF-8</code>
+     * is equivalent with calling
+     * <code>setContentType</code> with the <code>String</code> of
+     * <code>text/html; charset=UTF-8</code>.
+     * <p>This method can be called repeatedly to change the character
+     * encoding.
+     * This method has no effect if it is called after
+     * <code>getWriter</code> has been
+     * called or after the response has been committed.
+     * <p>Containers must communicate the character encoding used for
+     * the servlet response's writer to the client if the protocol
+     * provides a way for doing so. In the case of HTTP, the character
+     * encoding is communicated as part of the <code>Content-Type</code>
+     * header for text media types. Note that the character encoding
+     * cannot be communicated via HTTP headers if the servlet does not
+     * specify a content type; however, it is still used to encode text
+     * written via the servlet response's writer.
+     *
+     * @param charset a String specifying only the character set
+     * defined by IANA Character Sets
+     * (http://www.iana.org/assignments/character-sets)
+     *
+     * @see #setContentType
+     */
+    public void setCharacterEncoding(String charset);
+
+    /**
+     * Returns the content type used for the MIME body
+     * sent in this response. The content type proper must
+     * have been specified using {@link #setContentType}
+     * before the response is committed. If no content type
+     * has been specified, this method returns null.
+     * If a content type has been specified, and a
+     * character encoding has been explicitly or implicitly
+     * specified as described in {@link #getCharacterEncoding} has been called,
+     * the charset parameter is included in the string returned.
+     * If no character encoding has been specified, the
+     * charset parameter is omitted.
+     *
+     * @return a <code>String</code> specifying the content type,
+     * for example, <code>text/html; charset=UTF-8</code>, or null
+     */
+    public String getContentType();
+
+    /**
+     * Sets the content type of the response being sent to
+     * the client, if the response has not been committed yet.
+     * The given content type may include a character encoding
+     * specification, for example, <code>text/html;charset=UTF-8</code>.
+     * The response's character encoding is only set from the given
+     * content type if this method is called before <code>getWriter</code>
+     * is called.
+     * <p>This method may be called repeatedly to change content type and
+     * character encoding.
+     * This method has no effect if called after the response
+     * has been committed. It does not set the response's character
+     * encoding if it is called after <code>getWriter</code>
+     * has been called or after the response has been committed.
+     * <p>Containers must communicate the content type and the character
+     * encoding used for the servlet response's writer to the client if
+     * the protocol provides a way for doing so. In the case of HTTP,
+     * the <code>Content-Type</code> header is used.
+     *
+     * @param type a <code>String</code> specifying the MIME type of the content
+     * @see #setCharacterEncoding
+     * @see #getOutputStream
+     */
+    public void setContentType(String type);
+
+    /**
+     * Returns a {@link OutputStream} suitable for writing binary data in the response.
+     * The servlet container does not encode the binary data.
+     **
+     * @return a {@link OutputStream} for writing binary data
+     * @exception IOException if an input or output exception occurred
+     */
+    public OutputStream getOutputStream() throws IOException;
+
+    /**
+     * Sets the length of the content body in the response
+     * In HTTP servlets, this method sets the HTTP Content-Length header.
+     *
+     * @param len an integer specifying the length of the
+     * content being returned to the client; sets the Content-Length header
+     */
+    public void setContentLength(long len);
+
+    /**
+     * Returns a boolean indicating if the response has been committed.
+     * A committed response has already had its status code and headers written.
+     * @return a boolean indicating if the response has been committed
+     */
+    public boolean isCommitted();
+
+    /**
+     * Sends an error response to the client using the specified
+     * status and clears the buffer.  The server defaults to creating the
+     * response to look like an HTML-formatted server error page
+     * containing the specified message, setting the content type
+     * to "text/html". The server will preserve cookies and may clear or
+     * update any headers needed to serve the error page as a valid response.
+     *
+     * If an error-page declaration has been made for the web application
+     * corresponding to the status code passed in, it will be served back in
+     * preference to the suggested msg parameter and the msg parameter will
+     * be ignored.
+     *
+     * <p>If the response has already been committed, this method throws
+     * an IllegalStateException.
+     * After using this method, the response should be considered
+     * to be committed and should not be written to.
+     *
+     * @param    sc    the error status code
+     * @param    msg    the descriptive message
+     * @exception IOException    If an input or output exception occurs
+     * @exception IllegalStateException    If the response was committed
+     */
+    public void sendError(int sc, String msg) throws IOException;
+
+    /**
+     * Sends an error response to the client using the specified status
+     * code and clears the buffer.
+     *
+     * The server will preserve cookies and may clear or
+     * update any headers needed to serve the error page as a valid response.
+     *
+     * If an error-page declaration has been made for the web application
+     * corresponding to the status code passed in, it will be served back
+     * the error page
+     *
+     * <p>If the response has already been committed, this method throws
+     * an IllegalStateException.
+     * After using this method, the response should be considered
+     * to be committed and should not be written to.
+     *
+     * @param    sc    the error status code
+     * @exception IOException    If an input or output exception occurs
+     * @exception IllegalStateException    If the response was committed
+     *						before this method call
+     */
+    public void sendError(int sc) throws IOException;
+
+    /**
+     * Gets the current status code of this response.
+     * @return the current status code of this response
+     */
+    public int getStatus();
+
+    /**
+     * Returns a boolean indicating whether the named response header has already been set.
+     * @param    name    the header name
+     * @return <code>true</code> if the named response header has already been set; <code>false</code> otherwise
+     */
+    public boolean containsHeader(String name);
+
+    /**
+     *
+     * Sets a response header with the given name and value.
+     * If the header had already been set, the new value overwrites the
+     * previous one.  The <code>containsHeader</code> method can be
+     * used to test for the presence of a header before setting its
+     * value.
+     *
+     * @param    name    the name of the header
+     * @param    value    the header value  If it contains octet string,
+     *		it should be encoded according to RFC 2047
+     *		(http://www.ietf.org/rfc/rfc2047.txt)
+     *
+     * @see #containsHeader
+     * @see #addHeader
+     */
+    public void setHeader(String name, String value);
+
+    /**
+     * Adds a response header with the given name and value.
+     * This method allows response headers to have multiple values.
+     *
+     * @param    name    the name of the header
+     * @param    value    the additional header value   If it contains
+     *		octet string, it should be encoded
+     *		according to RFC 2047
+     *		(http://www.ietf.org/rfc/rfc2047.txt)
+     *
+     * @see #setHeader
+     */
+    public void addHeader(String name, String value);
+
+    /**
+     * Gets the value of the response header with the given name.
+     *
+     * <p>If a response header with the given name exists and contains
+     * multiple values, the value that was added first will be returned.
+     *
+     * <p>This method considers only response headers set or added via
+     * {@link #setHeader}, {@link #addHeader}, respectively.
+     *
+     * @param name the name of the response header whose value to return
+     * @return the value of the response header with the given name,
+     * or <tt>null</tt> if no header with the given name has been set
+     * on this response
+     */
+    public String getHeader(String name);
+
+    /**
+     * Gets the values of the response header with the given name.
+     *
+     * <p>This method considers only response headers set or added via
+     * {@link #setHeader}, {@link #addHeader}, respectively.
+     *
+     * <p>Any changes to the returned <code>Collection</code> must not
+     * affect this <code>HttpServletResponse</code>.
+     *
+     * @param name the name of the response header whose values to return
+     * @return a (possibly empty) <code>Collection</code> of the values
+     * of the response header with the given name
+     */
+    public Collection<String> getHeaders(String name);
+
+    /**
+     * Gets the names of the headers of this response.
+     *
+     * <p>This method considers only response headers set or added via
+     * {@link #setHeader}, {@link #addHeader}, respectively.
+     *
+     * <p>Any changes to the returned <code>Collection</code> must not
+     * affect this <code>HttpServletResponse</code>.
+     *
+     * @return a (possibly empty) <code>Collection</code> of the names
+     * of the headers of this response
+     *
+     * @since Servlet 3.0
+     */
+    public Collection<String> getHeaderNames();
+}
