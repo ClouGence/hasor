eb1ae25e4fa508b4480d8c2d64634a8da5e4f6ef
zyc <zyc@hasor.net>
2018-08-04 12:03:46 +0800

    1.增加 Hasor-boot 框架

---
+++ hasor-boot/src/main/java/net/hasor/boot/HasorLauncher.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.boot;
+import net.hasor.core.ApiBinder;
+import net.hasor.core.AppContext;
+import net.hasor.core.Hasor;
+import net.hasor.core.Module;
+import net.hasor.core.context.ContextShutdownListener;
+import net.hasor.core.context.ContextStartListener;
+import net.hasor.utils.ExceptionUtils;
+import net.hasor.utils.IOUtils;
+import net.hasor.utils.ResourcesUtils;
+import net.hasor.utils.StringUtils;
+import net.hasor.utils.future.BasicFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+/**
+ * 启动器，使用方法 HasorLauncher.run(MyModule.class,args);
+ * @version : 2018-08-04
+ * @author 赵永春 (zyc@hasor.net)
+ */
+public final class HasorLauncher implements Module, ContextStartListener, ContextShutdownListener {
+    private static Logger              logger    = LoggerFactory.getLogger(HasorLauncher.class);
+    private static BasicFuture<Object> future    = new BasicFuture<Object>();
+    static         String[]            mainArgs  = null;
+    static         boolean             usingBoot = false;
+    //
+    private HasorLauncher() {
+    }
+    public static void run(Class<? extends Module> launcherModuleType, String[] args) {
+        // .打印 Hello
+        try {
+            InputStream inputStream = ResourcesUtils.getResourceAsStream("/META-INF/hasor-framework/hello-text.txt");
+            List<String> helloText = IOUtils.readLines(inputStream, "utf-8");
+            for (String msg : helloText) {
+                logger.info(msg);
+            }
+            logger.info("");
+        } catch (Exception e) { /**/ }
+        mainArgs = args;
+        usingBoot = true;
+        //
+        // .确保有一个SetupModule
+        SetupModule setupModule = launcherModuleType.getAnnotation(SetupModule.class);
+        if (setupModule == null) {
+            setupModule = new SetupModule() {
+                public Class<? extends Annotation> annotationType() {
+                    return SetupModule.class;
+                }
+                public Class<? extends CreateBuilder> builder() {
+                    return DefaultCreateBuilder.class;
+                }
+                public String config() {
+                    return "hasor-config.xml";
+                }
+                public boolean join() {
+                    return false;
+                }
+            };
+        }
+        // .启动
+        try {
+            Class<? extends CreateBuilder> createBuilder = setupModule.builder();
+            CreateBuilder builder = createBuilder.newInstance();
+            Hasor hasor = builder.buildHasor(mainArgs);
+            String config = setupModule.config();
+            if (StringUtils.isNotBlank(config)) {
+                hasor.setMainSettings(config);
+            }
+            //
+            hasor.build(new HasorLauncher(), launcherModuleType.newInstance());
+            if (!setupModule.join()) {
+                future.completed(new Object());
+            }
+            future.get();
+        } catch (Exception e) {
+            throw ExceptionUtils.toRuntimeException(e);
+        }
+    }
+    public void loadModule(ApiBinder apiBinder) throws Throwable {
+        apiBinder.bindType(ContextStartListener.class).toInstance(this);
+        apiBinder.bindType(ContextShutdownListener.class).toInstance(this);
+        //
+    }
+    @Override
+    public void doStart(AppContext appContext) {
+    }
+    @Override
+    public void doStartCompleted(AppContext appContext) {
+        List<CommandLauncherDef> cmdSet = appContext.findBindingBean(CommandLauncherDef.class);
+        if (cmdSet == null || cmdSet.isEmpty()) {
+            return;
+        }
+        //
+        Collections.sort(cmdSet, new Comparator<CommandLauncherDef>() {
+            @Override
+            public int compare(CommandLauncherDef o1, CommandLauncherDef o2) {
+                int x = o1.getArgsIndex();
+                int y = o2.getArgsIndex();
+                return (x < y) ? -1 : ((x == y) ? 0 : 1);
+            }
+        });
+        //
+        for (CommandLauncherDef def : cmdSet) {
+            def.run(mainArgs, appContext);
+        }
+    }
+    @Override
+    public void doShutdown(AppContext appContext) {
+    }
+    @Override
+    public void doShutdownCompleted(AppContext appContext) {
+        future.completed(new Object());
+    }
+}
---
+++ hasor-boot/src/main/java/net/hasor/boot/SetupModule.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.boot;
+import java.lang.annotation.*;
+/**
+ * @version : 2018-08-04
+ * @author 赵永春 (zyc@hasor.net)
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+@Documented
+public @interface SetupModule {
+    /** Hasor 的 AppContext 生成器，用户构建各种不同环境的 AppContext */
+    public Class<? extends CreateBuilder> builder() default DefaultCreateBuilder.class;
+
+    /** 主配置文件名 */
+    public String config() default "hasor-config.xml";
+
+    /** 阻塞主进程的执行，直到收到一个停止信号为止。*/
+    public boolean join() default false;
+}
