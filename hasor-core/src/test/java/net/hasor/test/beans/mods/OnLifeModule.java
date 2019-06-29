139da4f0c56cd3fbc882e083223fcc257df4ae69
zyc <zyc@hasor.net>
2019-06-30 02:04:35 +0800

    1.所有测试 Bean 都放到 test 包下。

---
+++ hasor-core/src/test/java/net/hasor/test/beans/mods/OnLifeModule.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.test.beans.mods;
+import net.hasor.core.ApiBinder;
+import net.hasor.core.AppContext;
+import net.hasor.core.Module;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * 在模块中处理容器启动
+ * @version : 2013-9-14
+ * @author 赵永春 (zyc@byshell.org)
+ */
+public class OnLifeModule implements Module {
+    protected           Logger logger = LoggerFactory.getLogger(getClass());
+    public static final String STR    = "say form Mod_1.";
+    //
+    public void loadModule(ApiBinder apiBinder) throws Throwable {
+        apiBinder.installModule(new SimpleModule());
+        apiBinder.bindType(String.class).uniqueName().toInstance(STR);
+    }
+    @Override
+    public void onStart(AppContext appContext) throws Throwable {
+        logger.info("启动啦...");
+    }
+    @Override
+    public void onStop(AppContext appContext) throws Throwable {
+        logger.info("停止啦...");
+    }
+}
---
+++ hasor-core/src/test/java/net/hasor/test/beans/scope/MyScope.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.test.beans.scope;
+import net.hasor.core.Scope;
+import net.hasor.core.provider.SingleProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+/**
+ * 一个自定义 Scope ，实现了线程间单例化
+ * @version : 2015年11月9日
+ * @author 赵永春 (zyc@hasor.net)
+ */
+public class MyScope implements Scope {
+    private Map<Object, Supplier<?>> singtonMap = new HashMap<>();
+    //
+    public <T> Supplier<T> scope(Object key, final Supplier<T> provider) {
+        Supplier<?> returnData = singtonMap.get(key);
+        if (returnData == null) {
+            Supplier<T> newSingleProvider = new SingleProvider<T>(provider);
+            returnData = singtonMap.put(key, newSingleProvider);
+            if (returnData == null) {
+                returnData = newSingleProvider;
+            }
+        }
+        return (Supplier<T>) returnData;
+    }
+}
