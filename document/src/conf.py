efb8363b1b669b696dc6856ff48b8adf88f2473c
zyc <zyc@hasor.net>
2020-02-22 14:10:01 +0800

    1.遗漏了一个文件，版本号也改为最新。

--- document/src/conf.py
+++ document/src/conf.py
@@ -23,9 +23,9 @@ copyright = u'2020, 赵永春'
 author = u'赵永春'
 
 # The short X.Y version
-version = u'4.1.0'
+version = u'4.1.1'
 # The full version, including alpha/beta/rc tags
-release = u'Release.Hasor-4.1.0'
+release = u'Release.Hasor-4.1.1'
 
 
 # -- General configuration ---------------------------------------------------
