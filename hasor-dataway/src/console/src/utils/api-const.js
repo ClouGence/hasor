c57f06dd6da08d89165240741bb8eb5ea344ca7b
zyc <zyc@hasor.net>
2020-04-07 16:48:41 +0800

    1.report 暂时还不支持 https，因此有必要直接写死服务器端的请求协议。

--- hasor-dataway/src/console/src/utils/api-const.js
+++ hasor-dataway/src/console/src/utils/api-const.js
@@ -8,7 +8,7 @@ const adminBaseUrl = (oriUrl) => {
 };
 // 通用查 配置
 const ApiUrl = {
-    report: "//apis.hasor.net/projects/hasor-dataway/report",
+    report: "http://apis.hasor.net/projects/hasor-dataway/report",
     //
     apiInfo: adminBaseUrl(`/api/api-info`),
     apiList: adminBaseUrl(`/api/api-list`),
